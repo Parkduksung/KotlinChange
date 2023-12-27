18d5b49ccf310dcf44a1169b541b6de2befac3ef
Parkduksung <duksung1234@naver.com>
2023-12-27 22:08:51 +0900

    init

---
+++ app/src/main/java/com/example/kotlinchange/V1_3_70.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinchange
+
+/**
+ *
+ * Kotlin Version 1.3.70 추가 사항.
+ * Link : https://blog.jetbrains.com/kotlin/2020/03/kotlin-1-3-70-released/
+ */
+
+fun main() {
+    println("Hello, world!!!")
+}
+
+
