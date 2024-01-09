079274fc6af23e4f5d4610cb39ccd9b99e44f1aa
bagdeogseong <duksung1234@naver.com>
2024-01-09 21:51:58 +0900

    change 1.4.2

--- app/src/main/java/com/example/kotlinchange/V1_4_x.kt
+++ app/src/main/java/com/example/kotlinchange/V1_4_x.kt
@@ -1,5 +1,9 @@
 package com.example.kotlinchange
 
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
+
 // Kotlin 1.4.0
 
 //IDE 멈춤 현상 또는 메모리 누수를 유발하는 수많은 이슈를 포함하여 60여 개의 성능 이슈를 검토하고 수정.
@@ -88,7 +92,7 @@ fun functionSample() {
 fun applyInt(func: (Int) -> String): String = func(0)
 
 
-fun foo(f: () -> Unit) { }
+fun foo(f: () -> Unit) {}
 fun returnsInt(): Int = 42
 
 fun unitReturningFunctionsExam() {
@@ -98,7 +102,7 @@ fun unitReturningFunctionsExam() {
 
 //루프의 when 내에서 break 및 continue 사용
 fun testBefore_1_4(xs: List<Int>) {
-    LOOP@for (x in xs) {
+    LOOP@ for (x in xs) {
         when (x) {
             2 -> continue@LOOP
             17 -> break@LOOP
@@ -116,3 +120,58 @@ fun testStart_1_4(xs: List<Int>) {
         }
     }
 }
+
+
+//1.4.20
+// https://blog.jetbrains.com/ko/kotlin/2020/11/kotlin-1-4-20-released/
+
+
+//invokedynamic 문자열 연결
+
+fun invokeDynamicSample() {
+
+    val a = 1
+    val b = 2
+
+    println(a + b)
+    println(a.plus(b))
+
+
+    //-Xstring-concat 옵션 추가.
+//    println((a::plus)(b))
+
+}
+
+// 표준 라이브러리 변경 내용.
+
+// java.nio.file.Path용 확장 기능
+
+fun filePathSample() {
+
+    // 1.4.2에서 실험적 제공후 1.5에서 정식 제공.
+    val baseDir = Path("/base")
+
+    //list files in a directory
+    val kotlinFiles: List<Path> = Path("/home/user").listDirectoryEntries("*.kt")
+}
+
+// 내부적으로 new DirectoryStream<Path> 를 호출하고 여기서는 filter 처리하고 있는게 보인다.
+
+//public interface DirectoryStream<T> extends Closeable, Iterable<T> {
+//    Iterator<T> iterator();
+//
+//    @FunctionalInterface
+//    public interface Filter<T> {
+//        boolean accept(T var1) throws IOException;
+//    }
+//}
+
+// Kotlin Android Extensions 지원 중단
+
+// Kotlin synthetics 를 대체하는 뷰 바인딩이 생기면서 Kotlin Android Extensions 는 지원 중단. 원한다면 별도 플러그인 제공해줌.
+
+// Parcelable 구현 생성기는 이제 kotlin-parcelize 플러그인에서 사용할 수 있게 되어짐.
+
+
+
+
