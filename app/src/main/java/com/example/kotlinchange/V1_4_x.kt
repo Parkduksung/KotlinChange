7e7b4765cb7bf44851737d4f26ae895b854cbb9a
bagdeogseong <duksung1234@naver.com>
2024-01-09 22:30:47 +0900

    change 1.4.30

--- app/src/main/java/com/example/kotlinchange/V1_4_x.kt
+++ app/src/main/java/com/example/kotlinchange/V1_4_x.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinchange
 
+import android.graphics.Point
 import java.nio.file.Path
 import kotlin.io.path.Path
 import kotlin.io.path.listDirectoryEntries
@@ -173,5 +174,92 @@ fun filePathSample() {
 // Parcelable 구현 생성기는 이제 kotlin-parcelize 플러그인에서 사용할 수 있게 되어짐.
 
 
+//1.4.30
+// https://tangunsoft.com/tiptech/detail?id=201&cate=2&vendor=25&tag=&keyword=1.4.30
 
+// 언어 기능 테스트 버전
+
+// inline 클래스 베터 버전 승격.
+@JvmInline //1.5 부터 정식 지원.
+value class Name(private val s: String)
+
+//위와 같이 JVM backends 에서 사용한다.
+inline class _Name(private val s: String)
+
+
+@JvmInline
+value class Negative(val x: Int) {
+
+    // 1.4.30 부터는 init 블록을 추가할 수 있음. 클래스가 인스턴스화된 직후에 실행될 코드를 추가할 수 있어졌다.
+    init {
+        require(x < 0) { }
+    }
+}
+
+// JVM 레코드 지원
+// JDK 16 릴리즈에는 record 라는 새로운 Java 클래스를 코틀린에서 상호 운용성을 유지하기 위해 실험점 도입.
+// 최소 JDK 15 이상 필요.
+@JvmRecord
+data class User(val name: String, val age: Int)
+
+
+//sealed interface 실험적 지원.
+//sealed interface 는 sealed class 와 동일한 제한을 가지지만, 인터페이스로 선언된다는 점이 다르다.
+//더 유연한 sealed 된 (제한된) 계층 구조를 구축할 수 있게 도와준다.
+
+sealed interface Polygon
+
+class Rectangle() : Polygon
+class Triangle() : Polygon
+
+fun draw(polygon: Polygon) = when(polygon) {
+    is Rectangle -> println("Draw Rectangle")
+    is Triangle -> println("Draw Triangle")
+}
+
+sealed interface Fillable {
+    fun fill()
+}
+
+sealed interface Polygon2 {
+    val vertices : List<Point>
+}
+
+
+// sealed interface 를 사용하여 하나 이상의 실드 슈퍼 클래스에서 클래스를 상속할 수 있다.
+class Rectangle2(
+    override val vertices: List<Point>
+) : Polygon2, Fillable {
+    override fun fill() {
+        println("Fill Rectangle")
+    }
+}
+
+//Gradle 프로젝트 개선
+// Gradle 구성 캐시 지원 -> 후속 빌드 재사용 -> 빌드 프로세스 가속화.
+
+//대소문자 변환 API 실험적 추가.
+
+
+//String
+val a = "abc".uppercase() // ABC
+val b = "AbC".lowercase() // abc
+val c = "abc".replaceFirstChar { it.uppercase() } // Abc
+val d = "ABC".replaceFirstChar { it.lowercase() } // aBC
+
+//Char
+//uppercase(), lowercase() 및 titlecase()
+
+
+
+//문자를 숫자로 명확하게 변환하는 확장함수 추가.
+// digitToInt(radix : Int) , digitToIntOrNull(radix: Int), digitToChar(radix: Int)
+
+
+
+//kotlinx.serialization 1.1.0-RC 버전 출시
+//@Serializable 를 사용하여 inline 클래스 직렬화 지원.
+// 참고 : https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/value-classes.md
+
+//1.4.30부터 kotlinx.serialization의 표준 JSON 직렬화기를 사용하여 부호 없는 원시 유형(UInt, ULong, UByte 및 UShort)에 대해 사용할 수 있게 됨.
 
